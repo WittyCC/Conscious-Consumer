580fee1a6257effe9da40b56469c0a03a38d8296
Witty Chang <wcc1213@gmail.com>
2017-09-08 13:54:17 -0700

    Add About view and Profile view.

--- app/src/main/java/com/epicodus/conscioume/MainActivity.java
+++ app/src/main/java/com/epicodus/conscioume/MainActivity.java
@@ -7,19 +7,36 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button mFindItemButton;
+//    private Button mFindItemButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFindItemButton = (Button) findViewById(R.id.findItemButton);
-            mFindItemButton.setOnClickListener(new View.OnClickListener() {
+        Button center = (Button) findViewById(R.id.findItemButton);
+        Button left = (Button) findViewById(R.id.aboutButton);
+        Button right = (Button) findViewById(R.id.profileButton);
+
+        center.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Toast.makeText(MainActivity.this, "Welcome to Conscious Consumer", Toast.LENGTH_LONG).show();
                 }
-            });
+        });
+
+        left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Epicodus independent project for Android, September 8, 2017", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Your profile here", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
